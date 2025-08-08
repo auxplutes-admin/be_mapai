d108965d4b8a39592356ae81d138aa081af27d7b
rushib1699 <rushib777777@gmail.com>
2025-08-08 17:16:22 -0400

    cors

--- config/corsoptions.js
+++ config/corsoptions.js
@@ -1,7 +1,7 @@
 //const config = require('../APIs/config/serverConfig.json')
 
 const corsOptions = {
-    origin: ["http://localhost:3000","https://enttlevo.online", "https://www.enttlevo.online", "https://chs-repo.vercel.app", "https://ch-hut-repo.vercel.app", "https://hopelessly-usable-stag.ngrok-free.app", "https://dear-moccasin-vastly.ngrok-free.app", "https://enabled-akita-highly.ngrok-free.app", "https://dev.enttlevo.online"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://mapai.enttlevo.online"],
     credentials: true,
     methods: ["GET", "POST", "PUT"],
 };
